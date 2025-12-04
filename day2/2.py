fc2738ea7290c253de6108d00fcbb726069bc79a
Mikołaj Giza <93527772+gmikx@users.noreply.github.com>
2025-12-04 01:37:48 +0100

    day2 optimization pt1

--- day2/2.py
+++ day2/2.py
@@ -1,24 +1,62 @@
-f = open("./day2/input.txt",'r')
-ranges = f.readline()
-f.close()
+with open("./day2/input.txt") as f:
+    ranges_str = f.read().strip()
 
-ranges = ranges.split(",")
-ranges = [tuple(map(int, i.split("-"))) for i in ranges]
+raw_ranges = sorted(tuple(map(int, r.split("-"))) for r in ranges_str.split(","))
 
-def is_silly_id(id: str)->bool:
-    for i in range(1, len(id)):
-        running_count = id.count(id[:i])
+merged_ranges = []
+curr_start, curr_end = raw_ranges[0]
+for next_start, next_end in raw_ranges[1:]:
+    if next_start <= curr_end + 1:
+        curr_end = max(curr_end, next_end)
+    else:
+        merged_ranges.append((curr_start, curr_end))
+        curr_start, curr_end = next_start, next_end
+merged_ranges.append((curr_start, curr_end))
 
-        if(running_count==0):
-            return False
-        if running_count*i == len(id):
+global_max = merged_ranges[-1][1]
+max_digits = len(str(global_max))
+
+def is_periodic(n):
+    s = str(n)
+    l = len(s)
+    for i in range(1, l // 2 + 1):
+        if l % i == 0 and s[:i] * (l // i) == s:
             return True
+    return False
 
-id_sum=0
+total = 0
 
-for range_ in ranges:
-    for i in range(range_[0], range_[1]+1):
-        if is_silly_id(str(i)):
-            id_sum+=i
+for L in range(1, max_digits // 2 + 1):
+    min_seed = 10**(L - 1)
+    max_seed = 10**L - 1
+    
+    k = 2
+    while True:
+        multiplier = (10**(k * L) - 1) // (10**L - 1)
+        
+        min_val = min_seed * multiplier
+        max_val = max_seed * multiplier
+        
+        if min_val > global_max:
+            break
+        
+        for r_start, r_end in merged_ranges:
+            if r_end < min_val:
+                continue
+            if r_start > max_val:
+                break
+            
+            s_min = (r_start + multiplier - 1) // multiplier
+            s_max = r_end // multiplier
+            
+            start_s = max(min_seed, s_min)
+            end_s = min(max_seed, s_max)
+            
+            if start_s <= end_s:
+                for s in range(start_s, end_s + 1):
+                    if not is_periodic(s):
+                        total += s * multiplier
+        
+        k += 1
 
-print(id_sum)
+print(total)
